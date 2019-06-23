4b6a912a9087939f70fbe3ac8c586841c0c84e37
Nikolay Norkin <nikolay.norkin@forward3d.com>
2019-06-23 19:37:41 +0100

    desired follows amount and unfollowing with rotation

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ import time
 import schedule
 import random
 from instabot import Bot, utils
+from datetime import datetime, timedelta
 
 import config
 import credentials
@@ -36,25 +37,69 @@ bot = Bot(
 )
 bot.login(username = credentials.USERNAME, password = credentials.PASSWORD)
 
+existing_followers = utils.file(config.EXISTING_FOLLOWERS_FILE)
+processed_existing_followers = utils.file(config.PROCESSED_EXISTING_FOLLOWERS_FILE)
+
 def all_users():
     return bot.read_list_from_file(config.USERS_FILE)
 
 def random_user():
     return random.choice(all_users())
 
+def followed_file_name(date):
+    return "followed_{}".format(date.strftime("%Y%m%d"))
+
+def today():
+    return datetime.now()
+
+def three_days_ago():
+    return today() - timedelta(days = 3)
+
+def save_to_followed_today(user_id):
+    followed_today_file = utils.file(followed_file_name(today()))
+    followed_today_file.append(user_id)
+
 def follow_and_like(user_id):
     if not bot.follow(user_id):
         return False
+    save_to_followed_today(user_id)
     bot.like_user(user_id, amount = config.LIKES_COUNT, filtration = False)
+    return True
 
 def random_followers():
     user = random_user()
     bot.logger.info("Getting random followers of {}".format(user))
     return bot.get_user_followers(random_user(), config.FOLLOWERS_COUNT)
 
+def existing_followers_left():
+    return list(set(existing_followers.list) - set(processed_existing_followers.list))
+
+def random_existing_follower():
+    return random.choice(existing_followers_left())
+
+def next_existing_follower():
+    follower = random_existing_follower()
+    processed_existing_followers.append(follower)
+    return follower
+
 def process_followers():
+    total_processed = 0
+
     for follower in random_followers():
-        follow_and_like(follower)
+        if follow_and_like(follower):
+            total_processed += 1
+
+    while total_processed < config.FOLLOWERS_COUNT:
+        if follow_and_like(next_existing_follower()):
+            total_processed += 1
+
+def followed_3_days_ago():
+    filename = followed_file_name(three_days_ago())
+    utils.file(filename).list
+
+def unfollow_old():
+    bot.unfollow_users(followed_3_days_ago())
+
 
 # def run_threaded(job_fn):
 #     job_thread = threading.Thread(target=job_fn)
@@ -71,9 +116,4 @@ def process_followers():
 #     schedule.run_pending()
 #     time.sleep(1)
 
-# process_followers()
-
-existing_followers = utils.file(config.EXISTING_FOLLOWERS_FILE)
-for user in all_users():
-    for follower in bot.get_user_followers(user, None):
-        existing_followers.append(follower)
+process_followers()
--- config.py
+++ config.py
@@ -3,3 +3,4 @@ LIKES_COUNT=2
 MAX=99999
 USERS_FILE = "usersdb.txt"
 EXISTING_FOLLOWERS_FILE = "existing_followers.txt"
+PROCESSED_EXISTING_FOLLOWERS_FILE = "processed_existing_followers.txt"
